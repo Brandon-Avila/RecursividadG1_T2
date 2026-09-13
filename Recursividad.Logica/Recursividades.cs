fbbf4199d1b2eb86122a9cfa70c1fdc48de7623e
Brandon-Avila <25030027@itesa.edu.mc>
2026-09-13 16:07:42 -0600

    Version_02 (recursividades)

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorial(int num)
+        {
+            //Caso base condición de salida
+
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            //Caso general
+            return num = num * CalcularFactorial(num - 1);
+
+
+
+        }
+        public int Vocales(System.String cadena)
+        {
+            //Caso Base o salida
+            if(cadena.Length == 0)
+            {
+                return 0;
+            }
+            char letra=char.ToLower(cadena[0]);
+            int cont = 0;
+            if (letra== 'a' || letra== 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+            {
+                cont = 1;
+            }
+
+
+            //Caso general
+            return cont = cont + Vocales(cadena.Substring(1));
+
+        }
+
+        public int SumaDigitos(int digito)
+        {
+            //Caso Base o salida
+            
+            if (digito < 10)
+            {
+                return digito;
+            }
+            //Caso general
+            return digito = (digito % 10) + SumaDigitos(digito / 10);
+        }
+
+        public int SumarNumeros(int numero)
+        {
+            //Caso Base o salida
+            if(numero==1)
+            {
+                return 1;
+            }
+
+            //Caso general
+            return numero=numero + SumarNumeros(numero-1);
+        }
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,35 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Ejemplos de recursividad");
+//Console.WriteLine("\n\nFactorial");
+
+Recursividades oRecursividades = new Recursividades();
+
+//int resultadoFactorial = oRecursividades.CalcularFactorial(5);
+//Console.WriteLine("El resultado de 5! es: " + resultadoFactorial);
+
+Console.WriteLine("\n\nVocales");
+int resultadoVocales = oRecursividades.Vocales("Wakanda por siempre");
+Console.WriteLine("El numero de vocales en Wakanda por siempre es de: " + resultadoVocales);
+
+Console.WriteLine("\n\nSuma de digitos");
+int[] numeros = {123, 456, 209, 789, 9999 };
+int mayorNum = 0;
+int mayorSuma = -1;
+for (int i = 0; i < numeros.Length; i++)
+{
+    int numero = numeros[i];
+    int suma = oRecursividades.SumaDigitos(numero);
+    if (suma > mayorSuma)
+    {
+        mayorSuma = suma;
+        mayorNum = numero;
+    }
+}
+Console.WriteLine("El numero con la suma mayor es: "+ mayorNum);
+Console.WriteLine("La suma de los digitos es de: " + mayorSuma);
+
+
+Console.WriteLine("\n\nSuma de Positivos");
+int resultadoSuma = oRecursividades.SumarNumeros(6);
+Console.WriteLine("El resultado de la suma de los primeros 6 numeros positivos es de: " + resultadoSuma);
